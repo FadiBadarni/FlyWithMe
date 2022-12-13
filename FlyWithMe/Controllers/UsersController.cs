d34b0afca0932e47f7a0813d7ac7fd1e9093cd92
abada <abedallaSK>
2022-12-13 16:32:21 +0200

    login

--- FlyWithMe/Controllers/UsersController.cs
+++ FlyWithMe/Controllers/UsersController.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Firebase.Database;
+using Firebase.Database.Query;
+using FlyWithMe.Models;
 
 namespace FlyWithMe.Controllers
 {
     public class UsersController : Controller
     {
+       
+        private static string FirbaseLink = "https://myflight-db2b1-default-rtdb.firebaseio.com";
         // GET: Login
         public ActionResult Login()
         {
@@ -17,5 +23,41 @@ namespace FlyWithMe.Controllers
         {
             return View();
         }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult> Register(RegisterModel model)
+        {
+            if (model.Password != model.PasswordConfirm) return View();
+            var firebaseClient = new FirebaseClient(FirbaseLink);
+            var dbUsers = await firebaseClient.Child("Users").OnceAsync<RegisterModel>();
+            foreach (var user in dbUsers)
+            {
+
+                if(user.Object.Email==model.Email) { return View(); }
+            }
+            var result = await firebaseClient.Child("Users").PostAsync(model);
+
+           return Redirect("/Home"); 
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult> Login(LoginModel model)
+        {
+           
+            var firebaseClient = new FirebaseClient(FirbaseLink);
+            var dbUsers = await firebaseClient.Child("Users").OnceAsync<RegisterModel>();
+            foreach (var user in dbUsers)
+            {
+
+                if (user.Object.Email == model.Email &&
+                    user.Object.Password == model.Password) { return Redirect("/Home"); }
+              
+            }
+            return View();
+        }
+
+
+
     }
 }
